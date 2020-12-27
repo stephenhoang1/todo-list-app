868df9d6ca6c095cd6d720de545f5ded69b63601
stephenhoang1 <s.hoang17@gmail.com>
2020-12-27 15:50:10 -0800

    adding to github

---
+++ src/components/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Todo from './Todo' 
+
+const TodoList = ( {todos, setTodos, filteredTodos}  ) => {
+    return (
+        <div className="todo-container">
+        <ul className="todo-list">
+            {filteredTodos.map((todo) => (
+                <Todo 
+                todos={todos}  // pass down the state so that we can modify/delete each todo
+                setTodos={setTodos}
+                todo={todo}
+                key={todo.id} 
+                text={todo.text}
+                />
+            ))}
+           
+        </ul>
+
+      </div>
+    )
+} 
+
+export default TodoList
